9e591ea5acdf643003ab79a7966d3fe3f9c8a1f6
@Vinicius_Padua <vinicius.padua@tarvos.ag>
2024-07-18 13:11:13 -0300

    feat: add checkout module

---
+++ src/modules/checkout/repository/order.model.ts
@@ -0,0 +1,29 @@
+import {
+  Column,
+  DataType,
+  Model,
+  PrimaryKey,
+  Table,
+} from "sequelize-typescript";
+
+import Client from "../domain/client.entity";
+import Product from "../domain/product.entity";
+
+@Table({
+  tableName: "orders",
+  timestamps: false,
+})
+export class OrderModel extends Model {
+  @PrimaryKey
+  @Column({ allowNull: false })
+  id: string;
+
+  @Column({ allowNull: false })
+  status: string;
+
+  @Column({ allowNull: false, type: DataType.JSON })
+  client: Client;
+
+  @Column({ allowNull: false, type: DataType.JSON })
+  products: Product[];
+}
